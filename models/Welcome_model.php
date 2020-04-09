707a2a711c9f11ca93834142b263606baa881d94
saurabhshukla01 <ss7838094755@gmail.com>
2020-04-09 19:39:22 +0530

    added some files 2020 is 09/April/2020

--- models/Welcome_model.php
+++
@@ -1,9 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-class Welcome_model extends Prud_Model{
-
-    function __construct() {
-        parent::__construct();
-    }
-    
-}
